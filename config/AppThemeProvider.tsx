c207ec23de7bae698849a57ff26251fa36009a43
Lukas Liebeck <l.liebeck@protonmail.com>
2024-03-17 00:47:04 +0100

    Added responsiveFontSizes to the theme

--- config/AppThemeProvider.tsx
+++ config/AppThemeProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider, responsiveFontSizes } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
@@ -18,51 +18,50 @@ export const AppThemeProvider = ({
 }: {} & React.PropsWithChildren) => {
   const { mode } = useColorMode();
 
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: mode,
-        },
-        typography: {
-          fontFamily: roboto.style.fontFamily,
-        },
-        components: {
-          MuiCssBaseline: {
-            styleOverrides: {
-              body: {
-                scrollbarColor: "#6b6b6b #2b2b2b",
-                "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
-                  backgroundColor: "#2b2b2b",
+  const theme = useMemo(() => {
+    let createdTheme = createTheme({
+      palette: {
+        mode: mode,
+      },
+      typography: {
+        fontFamily: roboto.style.fontFamily,
+      },
+      components: {
+        MuiCssBaseline: {
+          styleOverrides: {
+            body: {
+              scrollbarColor: "#6b6b6b #2b2b2b",
+              "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+                backgroundColor: "#2b2b2b",
+              },
+              "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+                borderRadius: 8,
+                backgroundColor: "#6b6b6b",
+                minHeight: 24,
+                border: "3px solid #2b2b2b",
+              },
+              "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus":
+                {
+                  backgroundColor: "#959595",
                 },
-                "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
-                  borderRadius: 8,
-                  backgroundColor: "#6b6b6b",
-                  minHeight: 24,
-                  border: "3px solid #2b2b2b",
+              "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active":
+                {
+                  backgroundColor: "#959595",
                 },
-                "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus":
-                  {
-                    backgroundColor: "#959595",
-                  },
-                "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active":
-                  {
-                    backgroundColor: "#959595",
-                  },
-                "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
-                  {
-                    backgroundColor: "#959595",
-                  },
-                "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
-                  backgroundColor: "#2b2b2b",
+              "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover":
+                {
+                  backgroundColor: "#959595",
                 },
+              "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+                backgroundColor: "#2b2b2b",
               },
             },
           },
         },
-      }),
-    [mode]
-  );
+      },
+    });
+    return responsiveFontSizes(createdTheme);
+  }, [mode]);
 
   return (
     <ThemeProvider theme={theme}>
