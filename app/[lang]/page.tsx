d4b432999039bbe52dce4b42eeadf921bfbda1c4
Lukas Liebeck <l.liebeck@protonmail.com>
2025-12-07 12:07:09 +0100

    fix: wrong params type

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,7 +2,7 @@ import { Locale } from "@/config/i18n-config";
 import { redirect } from "next/navigation";
 
 export default async function IndexPage(props: {
-  params: Promise<{ lang: Locale }>;
+  params: Promise<{ lang: string }>;
 }) {
   const { lang } = await props.params;
 
